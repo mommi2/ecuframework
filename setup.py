4464bb97d6ce494f3c3353d51ab86075388ecbe7
tommasoviciani <49819683+tommasoviciani@users.noreply.github.com>
2020-05-27 11:40:30 +0200

    Added content in setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ecuframework",
+    version="0.0.1",
+    author="Tommaso Viciani",
+    author_email="vicianitommaso17@gmail.com",
+    description="The ECU framework is a project that wants to simplify writing for Raspberry applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tommasoviciani/ecuframework.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
